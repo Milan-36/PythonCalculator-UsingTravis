9fa890ac8ca86f2f8e835c0f50dcabd1ca847591
MilanPatel3628 <34170004+MilanPatel3628@users.noreply.github.com>
2021-07-11 20:24:39 -0400

    Update: CSVReader.py with absolute path.

--- src/CsvReader/CSVReader.py
+++ src/CsvReader/CSVReader.py
@@ -1,5 +1,7 @@
 import csv
 from pathlib import Path
+
+
 # from pprint import pprint
 
 
@@ -13,10 +15,10 @@ class CsvReader:
 
     def __init__(self, filepath):
         self.data = []
-        # relative = Path(filepath)
-        # absolute = relative.absolute()  # Travis need absolute path to find data file
-        # with open(absolute) as text_data:
-        with open(filepath) as text_data:
+        relative = Path(filepath)
+        absolute = relative.absolute()  # Travis need absolute path to find data file
+        with open(absolute) as text_data:
+            # with open(filepath) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
