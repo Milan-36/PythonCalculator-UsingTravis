11910bae92b22549c822aa9992de6bff374f99d9
MilanPatel3628 <mkp6@njit.edu>
2021-07-11 22:36:24 -0400

    Update: updated FindAbsolutePath.py

--- src/FileLibrary/FindAbsolutePath.py
+++ src/FileLibrary/FindAbsolutePath.py
@@ -2,5 +2,7 @@ from pathlib import Path
 
 
 def findAbsolutePath(filepath):
-    relative = Path(filepath)
-    return relative.absolute()  # Travis need absolute path to find data file
+    relative = Path(filepath)  # Load the filepath in the relative object.
+    # absolute() method will return absolute path of the  file.
+    # Travis need absolute path to find data file.
+    return relative.absolute()
