8e02ec44cc4c275b71320835dcabf94a6e828875
MilanPatel3628 <mkp6@njit.edu>
2021-07-12 03:40:58 -0400

    Feature: Throw an exception for divide by zero, valid Int, empty list exception in Mean.py

--- src/Statistics/Mean.py
+++ src/Statistics/Mean.py
@@ -2,8 +2,15 @@ from src.Calculator.Addition import addition
 from src.Calculator.Division import division
 
 
-def mean(data):
-    total_sum = 0
-    for num in data:
-        total_sum = addition(total_sum, num)
-    return division(len(data), total_sum)  # division will perform total_sum / len(data)
+def calculate_mean(data):
+    try:
+        total_sum = 0
+        for num in data:
+            total_sum = addition(total_sum, num)
+        return division(len(data), total_sum)  # division will perform total_sum / len(data)
+    except ZeroDivisionError:
+        print("You can't divide by zero.")
+    except ValueError:
+        print("No valid integer!")
+    except IndexError:
+        print("It's a empty list.")
