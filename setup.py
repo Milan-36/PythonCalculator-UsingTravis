91bd56ab1ad082279b0a7ba6cd544770349370db
MilanPatel3628 <mkp6@njit.edu>
2021-07-11 21:11:46 -0400

    Update: setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='Calculator',
       version='0.0.1',
       description='Its a calculator',
-      author='',
+      author='Milankumar Patel',
       author_email='',
       url='',
       packages=find_packages(),
