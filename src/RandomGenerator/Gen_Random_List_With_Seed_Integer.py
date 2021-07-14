da2ccd0f2d971bea26b98550e2bc3c1a6571ec7c
MilanPatel3628 <mkp6@njit.edu>
2021-07-14 03:32:44 -0400

    Task: Created Gen_Random_List_With_Seed_Integer.py

---
+++ src/RandomGenerator/Gen_Random_List_With_Seed_Integer.py
@@ -0,0 +1,13 @@
+from numpy import random
+
+
+def Gen_Random_List_With_Seed_Integer(start, end, length, seed):
+    # The getstate() method of the random module returns an object with the
+    # current internal state of the random number generator.
+    state = random.get_state()
+    random.seed(seed)
+    try:
+        random_int_list = random.randint(start, end, length)
+        return random_int_list
+    finally:
+        random.set_state(state)
